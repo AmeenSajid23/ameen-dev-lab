033f5cd7d78ad2bd57a186d5bbcefbce5bae3aed
AmeenSajid <ameensajid23@gmail.com>
2026-01-07 06:13:05 +0000

    Auto update 2026-01-07 06:13:05

---
+++ autosync.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd "$(dirname "$0")"
+
+while inotifywait -r -e modify,create,delete,move .; do
+    git add .
+    git commit -m "Auto update $(date '+%Y-%m-%d %H:%M:%S')" || true
+    git push origin main
+done
